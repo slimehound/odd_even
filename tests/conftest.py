a5d8390f35be3f2590c2457cd69e21a9b45a1cd0
Raphael <raphael@silvaziegler.com>
2020-08-14 20:12:06 +0100

    Added PyScaffold

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Dummy conftest.py for odd_even.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    https://pytest.org/latest/plugins.html
+"""
+
+# import pytest
