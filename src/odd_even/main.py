dab3613feb82bb93005ac1388b14c387745b83c0
Raphael <raphael@silvaziegler.com>
2020-08-15 14:05:36 +0100

    v1.0

--- src/odd_even/main.py
+++ src/odd_even/main.py
@@ -1,6 +1,6 @@
 n = int(input("Enter an integer: "))
 
 if (n % 2) == 0:
-    print("{0} is even" + str(n))
+    print("{0} is even".format(n))
 else:
-    print("{0} is odd" + str(n))
+    print("{0} is odd".format(n))
